2de3661a007d92f03a53ee0d836104be93e00707
Tushar Arora <tushar.toshi12@gmail.com>
2024-04-11 17:09:46 +0530

    changed locomotive import

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import AboutPage from "./components/AboutPage"
 import ClientsPage from "./components/ClientsPage"
 import EyesPage from "./components/EyesPage"
@@ -7,15 +8,19 @@ import LandingPage from "./components/LandingPage"
 import Marquee from "./components/Marquee"
 import ProjectPage from "./components/ProjectPage"
 import Navbar from "./components/navbar/Navbar"
-import LocomotiveScroll from 'locomotive-scroll';
-// import 'locomotive-scroll/bundled/locomotive-scroll.css'
 
 function App() {
-  const locomotiveScroll = new LocomotiveScroll();
+  useEffect(() => {
+    (
+      async () => {
+        const LocomotiveScroll = (await import('locomotive-scroll')).default;
+        const locomotiveScroll = new LocomotiveScroll();
+      }
+    )()
+  }, []);
 
   return (
-    <>
-      <div className="w-full min-h-screen bg-slate-50">
+    <div className="w-full min-h-screen bg-slate-50">
         <Navbar />
         <LandingPage />
         <Marquee />
@@ -25,8 +30,7 @@ function App() {
         <ClientsPage />
         <ProjectPage />
         <Footer />
-      </div>
-    </>
+    </div>
   )
 }
 
